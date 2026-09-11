e6645037ef0fd902bd45d925ba16651e328c7ee7
kedareswar12 <kedareswartiruveedhi@gmail.com>
2026-09-11 20:45:07 +0530

    operators

---
+++ practice/operators/practice1.js
@@ -0,0 +1,16 @@
+// // Arethimatic operators in js
+// x =5;
+// y=2;
+
+// console.log("addition" , x+y);
+// console.log("Multiplication" , x*y);
+// console.log("division" , x/y);
+// console.log("subraction" , x-y);
+// console.log("Modulo division" , x%y);
+
+
+// logical operators
+// AND  x  y = x * y
+// OR  x y = x + y
+// x = -x 
+
